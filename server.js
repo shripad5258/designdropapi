c2820db5e3d5e115dc0b0114353b1274e2881297
Unknown_tht <65358061+shripad5258@users.noreply.github.com>
2022-10-04 23:15:39 +0530

    commit

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const app = express();
 app.use(express.json());
 
 // app.use(cors());
-app.use(cors({origin: ['https://designdrop.onrender.com']}));
+// app.use(cors({origin: ['https://designdrop.onrender.com']}));
+app.use(cors({origin: '*'}))
 // {origin: ['http://localhost:8888', 'http://127.0.0.1:8888']}
 // cors({origin: ['http://localhost:8888', 'http://127.0.0.1:8888']})
 
